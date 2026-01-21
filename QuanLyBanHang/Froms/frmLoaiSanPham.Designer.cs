2b19cb7cf00c632032e7bca9130d549eb8f5cd85
Admin <Admin@MT0604>
2026-01-21 17:03:54 +0700

    1

--- QuanLyBanHang/Froms/frmLoaiSanPham.Designer.cs
+++ QuanLyBanHang/Froms/frmLoaiSanPham.Designer.cs
@@ -29,14 +29,14 @@
         private void InitializeComponent()
         {
             groupBox1 = new GroupBox();
-            label1 = new Label();
-            textBox1 = new TextBox();
-            btnThem = new Button();
-            btnSua = new Button();
-            btnXoa = new Button();
-            btnLuu = new Button();
-            bthHuyBo = new Button();
             btnThoat = new Button();
+            btnHuyBo = new Button();
+            btnLuu = new Button();
+            btnXoa = new Button();
+            btnSua = new Button();
+            btnThem = new Button();
+            txtTenLoai = new TextBox();
+            lbTenLoai = new Label();
             groupBox2 = new GroupBox();
             dataGridView = new DataGridView();
             ID = new DataGridViewTextBoxColumn();
@@ -49,82 +49,20 @@
             // groupBox1
             // 
             groupBox1.Controls.Add(btnThoat);
-            groupBox1.Controls.Add(bthHuyBo);
+            groupBox1.Controls.Add(btnHuyBo);
             groupBox1.Controls.Add(btnLuu);
             groupBox1.Controls.Add(btnXoa);
             groupBox1.Controls.Add(btnSua);
             groupBox1.Controls.Add(btnThem);
-            groupBox1.Controls.Add(textBox1);
-            groupBox1.Controls.Add(label1);
+            groupBox1.Controls.Add(txtTenLoai);
+            groupBox1.Controls.Add(lbTenLoai);
             groupBox1.Location = new Point(-2, 12);
             groupBox1.Name = "groupBox1";
             groupBox1.Size = new Size(807, 102);
             groupBox1.TabIndex = 0;
             groupBox1.TabStop = false;
-            groupBox1.Text = "Thông tin hãng sản xuất";
+            groupBox1.Text = "Thông tin loại sản phẩm";
             groupBox1.Enter += groupBox1_Enter;
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Location = new Point(24, 33);
-            label1.Name = "label1";
-            label1.Size = new Size(121, 15);
-            label1.TabIndex = 0;
-            label1.Text = "Tên hãng sản xuất (*):\r\n";
-            // 
-            // textBox1
-            // 
-            textBox1.Location = new Point(151, 30);
-            textBox1.Multiline = true;
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(628, 27);
-            textBox1.TabIndex = 1;
-            // 
-            // btnThem
-            // 
-            btnThem.Location = new Point(151, 63);
-            btnThem.Name = "btnThem";
-            btnThem.Size = new Size(85, 27);
-            btnThem.TabIndex = 2;
-            btnThem.Text = "Thêm";
-            btnThem.UseVisualStyleBackColor = true;
-            // 
-            // btnSua
-            // 
-            btnSua.Location = new Point(259, 63);
-            btnSua.Name = "btnSua";
-            btnSua.Size = new Size(85, 27);
-            btnSua.TabIndex = 3;
-            btnSua.Text = "Sửa";
-            btnSua.UseVisualStyleBackColor = true;
-            // 
-            // btnXoa
-            // 
-            btnXoa.Location = new Point(371, 63);
-            btnXoa.Name = "btnXoa";
-            btnXoa.Size = new Size(85, 27);
-            btnXoa.TabIndex = 4;
-            btnXoa.Text = "Xóa";
-            btnXoa.UseVisualStyleBackColor = true;
-            // 
-            // btnLuu
-            // 
-            btnLuu.Location = new Point(477, 63);
-            btnLuu.Name = "btnLuu";
-            btnLuu.Size = new Size(85, 27);
-            btnLuu.TabIndex = 6;
-            btnLuu.Text = "Lưu";
-            btnLuu.UseVisualStyleBackColor = true;
-            // 
-            // bthHuyBo
-            // 
-            bthHuyBo.Location = new Point(583, 63);
-            bthHuyBo.Name = "bthHuyBo";
-            bthHuyBo.Size = new Size(85, 27);
-            bthHuyBo.TabIndex = 7;
-            bthHuyBo.Text = "Hủy bỏ";
-            bthHuyBo.UseVisualStyleBackColor = true;
             // 
             // btnThoat
             // 
@@ -134,6 +72,74 @@
             btnThoat.TabIndex = 8;
             btnThoat.Text = "Thoát";
             btnThoat.UseVisualStyleBackColor = true;
+            btnThoat.Click += btnThoat_Click;
+            // 
+            // btnHuyBo
+            // 
+            btnHuyBo.Location = new Point(583, 63);
+            btnHuyBo.Name = "btnHuyBo";
+            btnHuyBo.Size = new Size(85, 27);
+            btnHuyBo.TabIndex = 7;
+            btnHuyBo.Text = "Hủy bỏ";
+            btnHuyBo.UseVisualStyleBackColor = true;
+            btnHuyBo.Click += bthHuyBo_Click;
+            // 
+            // btnLuu
+            // 
+            btnLuu.Location = new Point(477, 63);
+            btnLuu.Name = "btnLuu";
+            btnLuu.Size = new Size(85, 27);
+            btnLuu.TabIndex = 6;
+            btnLuu.Text = "Lưu";
+            btnLuu.UseVisualStyleBackColor = true;
+            btnLuu.Click += btnLuu_Click;
+            // 
+            // btnXoa
+            // 
+            btnXoa.Location = new Point(371, 63);
+            btnXoa.Name = "btnXoa";
+            btnXoa.Size = new Size(85, 27);
+            btnXoa.TabIndex = 4;
+            btnXoa.Text = "Xóa";
+            btnXoa.UseVisualStyleBackColor = true;
+            btnXoa.Click += btnXoa_Click;
+            // 
+            // btnSua
+            // 
+            btnSua.Location = new Point(259, 63);
+            btnSua.Name = "btnSua";
+            btnSua.Size = new Size(85, 27);
+            btnSua.TabIndex = 3;
+            btnSua.Text = "Sửa";
+            btnSua.UseVisualStyleBackColor = true;
+            btnSua.Click += btnSua_Click;
+            // 
+            // btnThem
+            // 
+            btnThem.Location = new Point(151, 63);
+            btnThem.Name = "btnThem";
+            btnThem.Size = new Size(85, 27);
+            btnThem.TabIndex = 2;
+            btnThem.Text = "Thêm";
+            btnThem.UseVisualStyleBackColor = true;
+            btnThem.Click += btnThem_Click;
+            // 
+            // txtTenLoai
+            // 
+            txtTenLoai.Location = new Point(151, 30);
+            txtTenLoai.Multiline = true;
+            txtTenLoai.Name = "txtTenLoai";
+            txtTenLoai.Size = new Size(628, 27);
+            txtTenLoai.TabIndex = 1;
+            // 
+            // lbTenLoai
+            // 
+            lbTenLoai.AutoSize = true;
+            lbTenLoai.Location = new Point(24, 33);
+            lbTenLoai.Name = "lbTenLoai";
+            lbTenLoai.Size = new Size(121, 15);
+            lbTenLoai.TabIndex = 0;
+            lbTenLoai.Text = "Tên loại sản phẩm (*):\r\n";
             // 
             // groupBox2
             // 
@@ -162,17 +168,18 @@
             // 
             // ID
             // 
+            ID.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
             ID.DataPropertyName = "ID";
             ID.HeaderText = "ID";
             ID.Name = "ID";
+            ID.Width = 43;
             // 
             // TenLoai
             // 
-            TenLoai.AutoSizeMode = DataGridViewAutoSizeColumnMode.None;
+            TenLoai.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
             TenLoai.DataPropertyName = "TenLoai";
             TenLoai.HeaderText = "Tên loại sản phẩm";
             TenLoai.Name = "TenLoai";
-            TenLoai.Width = 395;
             // 
             // frmLoaiSanPham
             // 
@@ -183,7 +190,7 @@
             Controls.Add(groupBox1);
             Name = "frmLoaiSanPham";
             Text = "Loại Sản Phẩm";
-            Load += this.frmLoaiSanPham_Load;
+            Load += frmLoaiSanPham_Load;
             groupBox1.ResumeLayout(false);
             groupBox1.PerformLayout();
             groupBox2.ResumeLayout(false);
@@ -194,10 +201,10 @@
         #endregion
 
         private GroupBox groupBox1;
-        private TextBox textBox1;
-        private Label label1;
+        private TextBox txtTenLoai;
+        private Label lbTenLoai;
         private Button btnThoat;
-        private Button bthHuyBo;
+        private Button btnHuyBo;
         private Button btnLuu;
         private Button btnXoa;
         private Button btnSua;
--- QuanLyBanHang/Froms/frmLoaiSanPham.cs
+++ QuanLyBanHang/Froms/frmLoaiSanPham.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using QuanLyBanHang.Data;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,6 +13,9 @@ namespace QuanLyBanHang.Froms
 {
     public partial class frmLoaiSanPham : Form
     {
+        QLBHDbContext context = new QLBHDbContext();  // Khởi tạo biến ngữ cảnh CSDL
+        bool xuLyThem = false;  // Kiểm tra có nhấn vào nút Thêm hay không?
+        int id;  // Lấy mã loại sản phẩm (dùng cho Sửa và Xóa)
         public frmLoaiSanPham()
         {
             InitializeComponent();
@@ -38,6 +42,94 @@ namespace QuanLyBanHang.Froms
         }
 
         private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            xuLyThem = true;
+            BatTatChucNang(true);
+            txtTenLoai.Clear();
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            xuLyThem = false;
+            BatTatChucNang(true);
+            id = Convert.ToInt32(dataGridView.CurrentRow.Cells["ID"].Value.ToString());
+        }
+
+        private void btnLuu_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(lbTenLoai.Text))
+                MessageBox.Show("Vui lòng nhập tên loại sản phẩm?", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            else
+            {
+                if (xuLyThem)
+                {
+                    LoaiSanPham lsp = new LoaiSanPham();
+                    lsp.TenLoai = lbTenLoai.Text;
+                    context.LoaiSanPham.Add(lsp);
+                    context.SaveChanges();
+                }
+                else
+                {
+                    LoaiSanPham lsp = context.LoaiSanPham.Find(id);
+                    if (lsp != null)
+                    {
+                        lsp.TenLoai = lbTenLoai.Text;
+                        context.LoaiSanPham.Update(lsp);
+                        context.SaveChanges();
+                    }
+                }
+                frmLoaiSanPham_Load(sender, e);
+            }
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Xác nhận xóa loại sản phẩm?", "Xóa", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                id = Convert.ToInt32(dataGridView.CurrentRow.Cells["ID"].Value.ToString());
+                LoaiSanPham lsp = context.LoaiSanPham.Find(id);
+                if (lsp != null)
+                {
+                    context.LoaiSanPham.Remove(lsp);
+                }
+                context.SaveChanges();
+                frmLoaiSanPham_Load(sender, e);
+            }
+        }
+
+        private void bthHuyBo_Click(object sender, EventArgs e)
+        {
+            frmLoaiSanPham_Load(sender, e);
+        }
+
+        private void BatTatChucNang(bool giaTri)
+        {
+            btnLuu.Enabled = giaTri;
+            btnHuyBo.Enabled = giaTri;
+            lbTenLoai.Enabled = giaTri;
+            btnThem.Enabled = !giaTri;
+            btnSua.Enabled = !giaTri;
+            btnXoa.Enabled = !giaTri;
+        }
+
+        private void frmLoaiSanPham_Load(object sender, EventArgs e)
+        {
+            BatTatChucNang(false);
+            List<LoaiSanPham> lsp = new List<LoaiSanPham>();
+            lsp = context.LoaiSanPham.ToList();
+            BindingSource bindingSource = new BindingSource();
+            bindingSource.DataSource = lsp;
+            lbTenLoai.DataBindings.Clear();
+            lbTenLoai.DataBindings.Add("Text", bindingSource, "TenLoai", false, DataSourceUpdateMode.Never);
+            dataGridView.DataSource = bindingSource;
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
         {
 
         }
